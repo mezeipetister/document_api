955b3ece274526afd206c6ef90a61c71a52375f9
Peter Mezei <mezeipetister@gmail.com>
2018-05-26 13:50:51 +0200

    license headers added

--- main.go
+++ main.go
@@ -4,6 +4,18 @@
  *
  * License AGPL v3.0
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  * For more information please contact me
  * via github.com
  */
@@ -11,5 +23,5 @@
 package main
 
 func main() {
-
+	// TODO: server + router + logging + error handling
 }
--- user/user.go
+++ user/user.go
@@ -1,3 +1,25 @@
+/*
+ * Created on Sat May 26 2018
+ * Copyright (c) 2018 Peter Mezei
+ *
+ * License AGPL v3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * For more information please contact me
+ * via github.com
+ */
+
 package user
 
 import (
@@ -63,7 +85,7 @@ func (u *Model) Save() error {
 
 // Remove the current user document from database
 func (u *Model) Remove() error {
-	u.datastore.RemoveDocumentById(u.Get().ID)
+	u.datastore.RemoveDocumentByID(u.Get().ID)
 	return nil
 }
 
